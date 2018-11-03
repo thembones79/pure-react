b6d5febc357d9602cd2d0e0e9707da939bbbbe83
thembones79 <michal.szumnarski@gmail.com>
2018-11-03 19:32:26 +0100

    learning React Fragment

--- react-hello/src/index.js
+++ react-hello/src/index.js
@@ -2,14 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 function HelloWorld() {
-    return React.createElement(
-        'div',
-        {},
-        'Hello',
-        'World!'
-        
+    return (
+        <React.Fragment>
+            <Hello/>
+            <World/>
+        </React.Fragment>       
     );
 }
+
+function Hello(){
+    return (
+        <span>Hello</span>
+    );
+}
+
+function World(){
+    return(
+        <span>World!</span>
+    );
+}
+
 
 ReactDOM.render(
     <HelloWorld/>,
