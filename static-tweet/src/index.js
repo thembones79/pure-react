1378c834704d44b419170f146f811ab0ed9c9843
thembones79 <michal.szumnarski@gmail.com>
2018-11-04 22:03:12 +0100

    first commit

---
+++ static-tweet/src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css'
+
+function Tweet() {
+    return (
+        <div className="tweet">
+            <Avatar/>
+            Tweet
+        </div>
+    );
+}
+
+function Avatar() {
+    return (
+        <img
+        src="https://www.gravatar.com/avatar/nothing"
+        className="avatar"
+        alt="avatar" />
+    );
+}
+
+ReactDOM.render(<Tweet/>, document.querySelector('#root'));
