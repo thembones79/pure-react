8b830f853b7b1babe3390adf7b1657b9707f4ad3
thembones79 <michal.szumnarski@gmail.com>
2018-11-04 23:00:44 +0100

    nice looking static tweet

--- static-tweet/src/index.js
+++ static-tweet/src/index.js
@@ -5,8 +5,17 @@ import './index.css'
 function Tweet() {
     return (
         <div className="tweet">
-            <Avatar/>
-            Tweet
+            <Avatar />
+            <div className="cotent">
+                <NameWithHandle /><Time />
+                <Message />
+                <div className="buttons">
+                    <ReplyButton />
+                    <RetweetButton />
+                    <LikeButton />
+                    <MoreOptionsButton />
+                </div>
+            </div>
         </div>
     );
 }
@@ -14,10 +23,47 @@ function Tweet() {
 function Avatar() {
     return (
         <img
-        src="https://www.gravatar.com/avatar/nothing"
-        className="avatar"
-        alt="avatar" />
+            src="https://www.gravatar.com/avatar/nothing"
+            className="avatar"
+            alt="avatar" />
     );
 }
 
-ReactDOM.render(<Tweet/>, document.querySelector('#root'));
+function Message() {
+    return (
+        <div className="message">
+            This is less than 140 characters.
+        </div>
+    );
+}
+
+function NameWithHandle() {
+    return (
+        <span className="name-with-handle">
+            <span className="name">Your Name</span>
+            <span className="handle">@yourhandle</span>
+        </span>
+    );
+}
+
+const Time = () => (
+    <span className="time">3h ago</span>
+);
+
+const ReplyButton = () => (
+    <i className="fa fa-reply reply-button" />
+);
+
+const RetweetButton = () => (
+    <i className="fa fa-retweet retweet-button" />
+);
+
+const LikeButton = () => (
+    <i className="fa fa-heart like-button" />
+);
+
+const MoreOptionsButton = () => (
+    <i className="fa fa-ellipsis-h more-options-button" />
+);
+
+ReactDOM.render(<Tweet />, document.querySelector('#root'));
