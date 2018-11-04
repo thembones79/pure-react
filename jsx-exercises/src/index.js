7dbd8e30eae6e1e0fbc80fa011e3b7de575b6274
thembones79 <michal.szumnarski@gmail.com>
2018-11-04 20:07:31 +0100

    first commit

---
+++ jsx-exercises/src/index.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function MyThing() {
+    return (
+        <div className='book' >
+            <div className='title' >
+                The
+
+
+
+                Title
+                </div>
+            <div className='author' >
+                The Author
+                </div>
+
+                <div>
+&nbsp;Non-breaking
+&nbsp;Spaces&nbsp;
+</div>
+
+
+<div>
+Line1
+{' '}
+Line2
+</div>
+
+
+
+
+            <ul className='stats' >
+                <li className='rating' >
+                    5 stars
+                            </li>
+                <li className='isbn' >
+                    12-3456      78-910
+                </li>
+            </ul>
+        </div>
+    );
+
+}
+
+function Greeting() {
+    // Try all of these variations:
+    let username = "root";
+    //let username = undefined;
+    //let username = null;
+    //let username = false;
+    // Fill in the rest:
+    return (
+        <div>{username ? `Hello, ${username}` : `Not logged in`}</div>
+    );
+    }
+
+function TestComponent() {
+    return(
+    
+     
+        
+    );
+}
+
+ReactDOM.render(<TestComponent />, document.getElementById('root'));
