fa62134b8d03340cf8168aec9940455fbb43eb69
thembones79 <michal.szumnarski@gmail.com>
2018-11-07 22:18:57 +0100

    first commit

---
+++ envelope/src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const AddressLabel = ({theMan}) => (
+    <div className="address-label">
+        <p>{theMan.name}</p>
+        <p>{theMan.address}</p>
+        <p>{theMan.city}, {theMan.state} {theMan.zip}</p>
+    </div>
+);
+
+var person = {
+    name: "Full Name",
+    address: "123 Fake St.",
+    city: "Boston",
+    state: "MA",
+    zip: "02118",
+    country: "USA"
+};
+
+
+const Envelope = () => (
+    
+);
+
+ReactDOM.render(<AddressLabel theMan={person}/>, document.querySelector('#root'));
