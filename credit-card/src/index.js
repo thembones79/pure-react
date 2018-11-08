c6edc49ffce390ea18272c2db47c1630024107db
thembones79 <michal.szumnarski@gmail.com>
2018-11-08 23:39:24 +0100

    cleanup

--- credit-card/src/index.js
+++ credit-card/src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const CreditCard = ({cardInfo}) => (
+    <div>
+        Bank name,
+        Credit card number,
+        Four digits, VALID THRU, Expiration date
+        Person name
+    </div>
+)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<CreditCard />, document.querySelector('#root'));
