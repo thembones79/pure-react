7db9f853a2793044c448127d7a11966f1c1c4d39
thembones79 <michal.szumnarski@gmail.com>
2018-11-09 22:49:39 +0100

    v1.0

--- credit-card/src/index.js
+++ credit-card/src/index.js
@@ -2,13 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css'
 
-const CreditCard = ({cardInfo}) => (
-    <div>
-        Bank name,
-        Credit card number,
-        Four digits, VALID THRU, Expiration date
-        Person name
+
+const CreditCard = ({ information }) => (
+    <div className="credit-card">
+        <div className="bank-name">{information.bankName}</div>
+        <div className="card-number">{information.cardNumber}</div>
+        <div className="theThirdRow">
+            <div className="four-digits">{getDigits(information.cardNumber)}</div>
+            <div className="valid-thru">valid thru</div>
+            <div className="exp-date">{information.expDate}</div>
+        </div>
+        <div className="person-name">{information.personName}</div>
     </div>
 )
 
-ReactDOM.render(<CreditCard />, document.querySelector('#root'));
+var cardInfo = {
+    bankName: "Big Bank, Inc.",
+    cardNumber: "1234 5678 8765 4321",
+    expDate: "09/19",
+    personName: "Cardholder Name"
+};
+
+function getDigits(cardNumberString) {
+    var fourDigits = cardNumberString.slice(0, 4);
+    return (
+        fourDigits
+    );
+}
+
+ReactDOM.render(<CreditCard information={cardInfo} />, document.querySelector('#root'));
