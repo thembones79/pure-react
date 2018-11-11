aa36b1c9e69f1ca86ea28a5729892034fefbaaf0
thembones79 <michal.szumnarski@gmail.com>
2018-11-11 20:36:49 +0100

    pasted modal html code from w3schools

--- dialog/src/App.js
+++ dialog/src/App.js
@@ -1,7 +1,35 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
-const App = () => <div> Hi </div>;
+const App = () => (
+  <div class="container">
+    <button
+      type="button"
+      class="btn btn-primary"
+      data-toggle="modal"
+      data-target="#myModal"
+    >
+      Open modal
+    </button>
+    <div class="modal fade" id="myModal">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h4 class="modal-title">Modal Heading</h4>
+            <button type="button" class="close" data-dismiss="modal">
+              &times;
+            </button>
+          </div>
+          <div class="modal-body">Modal body..</div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-danger" data-dismiss="modal">
+              Close
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+);
 
 export default App;
