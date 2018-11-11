f5077f0e62b716fc2094242995cd02935a1d84a3
thembones79 <michal.szumnarski@gmail.com>
2018-11-11 12:50:47 +0100

    did the exercises

--- react-children-exercises/src/index.js
+++ react-children-exercises/src/index.js
@@ -1,24 +1,63 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function Nav({ children }) {
+const NavItem = ({ url, children }) => (
+    <a href={url} alt={children}>{children}</a>
+);
+
+function Nav1({ children }) {
     let items = React.Children.toArray(children);
-    for ( let i = items.length - 1; i >= 1; i -- ) {
-    items.splice(i, 0,
-    <span key={i} className='separator' > | </span>
-    );
+    for (let i = items.length - 1; i >= 1; i--) {
+        items.splice(i, 0,
+            <span key={i} className='separator' > | </span>
+        );
     }
     return (
-    <div> {items} </div>
+        <div> {items} </div>
     );
-    }
+}
 
-<Nav>
-<NavItem url='/' > Home </NavItem>
-<NavItem url='/about' > About </NavItem>
-<NavItem url='/contact' > Contact </NavItem>
-</Nav>
+const Exercise1 = () => (
+    <Nav1>
+        <NavItem url='/' > Home </NavItem>
+        <NavItem url='/about' > About </NavItem>
+        <NavItem url='/contact' > Contact </NavItem>
+    </Nav1>
+);
 
-ReactDOM.render(<Nav/>, document.getElementById('root'));
+function Nav2({ children }) {
+    let items = React.Children.toArray(children);
 
-    
+    return (
+        <div> {items[0]} </div>
+    );
+}
+
+const Exercise2 = () => (
+    <Nav2>
+        <NavItem url='/' > Home </NavItem>
+        <NavItem url='/about' > About </NavItem>
+        <NavItem url='/contact' > Contact </NavItem>
+    </Nav2>
+);
+
+function Nav3({ children }) {
+    let items = React.Children.toArray(children);
+    let promptNumOfChildren = prompt("Please enter number of children", "2");
+    let numOfChildren = promptNumOfChildren - 0;
+
+    return (
+        <div> {items.slice(0,numOfChildren)} </div>
+    );
+}
+
+const Exercise3 = () => (
+    <Nav3>
+        <NavItem url='/' > Home </NavItem>
+        <NavItem url='/about' > About </NavItem>
+        <NavItem url='/contact' > Contact </NavItem>
+    </Nav3>
+);
+
+ReactDOM.render(<Exercise3 />, document.getElementById('root'));
+
