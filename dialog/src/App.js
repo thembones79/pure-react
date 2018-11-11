c505e1bd76b0145f6bf152c30335a42efed4310c
thembones79 <michal.szumnarski@gmail.com>
2018-11-11 21:36:44 +0100

    works

--- dialog/src/App.js
+++ dialog/src/App.js
@@ -1,35 +1,55 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 import "./App.css";
 
 const App = () => (
-  <div class="container">
+  <div className="container">
     <button
       type="button"
-      class="btn btn-primary"
+      className="btn btn-primary"
       data-toggle="modal"
       data-target="#myModal"
     >
       Open modal
     </button>
-    <div class="modal fade" id="myModal">
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h4 class="modal-title">Modal Heading</h4>
-            <button type="button" class="close" data-dismiss="modal">
-              &times;
-            </button>
-          </div>
-          <div class="modal-body">Modal body..</div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-danger" data-dismiss="modal">
-              Close
-            </button>
-          </div>
-        </div>
-      </div>
+
+    <Dialog>
+      <Title>This Is Important</Title>
+      <Body>Here is some important text or error or something.</Body>
+      <Footer />
+    </Dialog>
+  </div>
+);
+
+const Title = ({ children }) => (
+  <div className="modal-header">
+    <h4 className="modal-title">{children}</h4>
+    <button type="button" className="close" data-dismiss="modal">
+      &times;
+    </button>
+  </div>
+);
+
+const Body = ({ children }) => <div className="modal-body">{children}</div>;
+
+const Footer = () => (
+  <div className="modal-footer">
+    <button type="button" className="btn btn-danger" data-dismiss="modal">
+      Close
+    </button>
+  </div>
+);
+
+const Dialog = ({ children }) => (
+  <div className="modal fade" id="myModal">
+    <div className="modal-dialog">
+      <div className="modal-content">{children}</div>
     </div>
   </div>
 );
+
+Dialog.propTypes = {
+  children: PropTypes.node.isRequired
+};
 
 export default App;
