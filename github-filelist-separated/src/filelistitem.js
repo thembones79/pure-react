9e0de5774f8c83273d03ab4b650f50055a3e3ffb
thembones79 <michal.szumnarski@gmail.com>
2018-11-12 21:25:19 +0100

    import correction

--- github-filelist-separated/src/filelistitem.js
+++ github-filelist-separated/src/filelistitem.js
@@ -1,6 +1,9 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Time from "./time";
+import FileIcon from "./fileicon";
+import FileName from "./filename";
+import CommitMessage from "./commitmessage"
 
 const FileListItem = ({ file }) => (
   <tr className="file-list-item">
