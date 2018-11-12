8ae0eaf0f0746817caac259286f2043ced4ada55
thembones79 <michal.szumnarski@gmail.com>
2018-11-12 21:26:25 +0100

    reformatted

--- github-filelist-separated/src/filelistitem.js
+++ github-filelist-separated/src/filelistitem.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 import Time from "./time";
 import FileIcon from "./fileicon";
 import FileName from "./filename";
-import CommitMessage from "./commitmessage"
+import CommitMessage from "./commitmessage";
 
 const FileListItem = ({ file }) => (
   <tr className="file-list-item">
