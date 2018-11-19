39d07e3a6b814d5ad483fafe1c5d9e23a6af626d
thembones79 <michal.szumnarski@gmail.com>
2018-11-19 22:33:05 +0100

    throws an error!?! wtf

---
+++ shopper/src/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Nav = () => (
+    <nav className="App-nav">
+        <ul>
+            <li className="App-nav-item"><span>Items</span></li>
+            <li className="App-nav-item"><span>Cart</span></li>
+        </ul>
+    </nav>
+);
+
+export default Nav;
