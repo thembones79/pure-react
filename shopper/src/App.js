ec43787f4418ff735af8455c1e12dc7e3f693862
thembones79 <michal.szumnarski@gmail.com>
2018-11-19 21:26:49 +0100

    first commit

---
+++ shopper/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Nav from "./Nav";
+import "./App.css";
+
+class App extends React.Components {
+  renderContent() {
+    return <span>Empty</span>;
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Nav />
+        <main className="App-content">{this.renderContent()}</main>
+      </div>
+    );
+  }
+}
+
+export default App;
