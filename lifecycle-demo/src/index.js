316c98c52e11b488fed741c5da200a9a7f56a563
thembones79 <michal.szumnarski@gmail.com>
2018-11-19 21:12:48 +0100

    last

--- lifecycle-demo/src/index.js
+++ lifecycle-demo/src/index.js
@@ -1,63 +1,92 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
 class ErrorCatcher extends React.Component {
-    state = { error: null }
+  state = { error: null };
 
-    componentDidCatch(error, info) {
-        console.log('[componentDidCatch]', error);
-        this.setState({error: info.componentStack});
-    }
+  componentDidCatch(error, info) {
+    console.log("[componentDidCatch]", error);
+    this.setState({ error: info.componentStack });
+  }
 
-    render() {
-        if(this.setState.error) {
-            return (
-                <div>
-                    An error occurred: {this.stste.error}
-                </div>
-            );
-        }
-        return this.props.children;
+  render() {
+    if (this.setState.error) {
+      return <div>An error occurred: {this.stste.error}</div>;
     }
+    return this.props.children;
+  }
 }
 
 class LifecycleDemo extends React.Component {
-    state = {counter: 0};
+  state = { counter: 0 };
 
-    constructor(props) {
-        super(props);
-        console.log('[constructor]');
-        console.log(' State already set:', this.state);
+  constructor(props) {
+    super(props);
+    console.log("[constructor]");
+    console.log(" State already set:", this.state);
+  }
+
+  componentDidMount() {
+    console.log("[componentDidMount]", "Mounted");
+  }
+
+  static getDerivedSateFromProps(nextProps, prevState) {
+    console.log("[getDerivedStateFromProps]");
+    console.log(" Next props:", nextProps);
+    console.log(" Prev state:", prevState);
+    return null;
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log("[shouldComponentUpdate]", "Deciding to update");
+    return true;
+  }
+
+  getSnapshotBeforeUpdate(nextProps, nextState) {
+    console.log("[getSnapshotBeforeUpdate]", "About to update...");
+    return `Time is ${Date.now()}`;
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log("[componentDidUpdate]", "Updated.");
+    console.log(" snapshot:", snapshot);
+  }
+
+  componentWillUnmount() {
+    console.log("{componentWillUnmout]", "Goodbye cruel world.");
+  }
+
+  handleClick = () => {
+    this.setState({
+      counter: this.state.counter + 1
+    });
+  };
+
+  causeErrorNextRender = () => {
+    this.setState({
+      causeError: true
+    });
+  };
+
+  render() {
+    console.log("[render]");
+    if (this.state.causeError) {
+      throw new Error("oh no!!");
     }
 
-    componentDidMount() {
-        console.log('[componentDidMount]', 'Mounted');
-    }
-
-    static getDerivedSateFromProps(nextProps, prevState) {
-        console.log('[getDerivedStateFromProps]');
-        console.log(' Next props:', nextProps);
-        console.log(' Prev state:', prevState);
-        return null;
-    }
-
-    shouldComponentUpdate(nextProps, nextState) {
-        console.log('[shouldComponentUpdate]', 'Deciding to update');
-        return true;
-    }
-
-    getSnapshotBeforeUpdate(nextProps, nextState) {
-        console.log('[getSnapshotBeforeUpdate]', 'About to update...');
-        return `Time is ${Date.now()}`;
-    }
-
-    componentDidUpdate(prevProps, prevState, snapshot) {
-        console.log('[componentDidUpdate]', 'Updated.');
-        console.log(' snapshot:', snapshot)
-    }
-
-    componentWillUnmount(){
-        console.log('{componentWillUnmout]', 'Goodbye cruel world.');
-    }
+    return (
+      <div>
+        <span>Counter: {this.state.counter}</span>
+        <button onClick={this.handleClick}>Click to increment</button>
+        <button onClick={this.causeErrorNextRender}>Throw an error</button>
+      </div>
+    );
+  }
 }
 
+ReactDOM.render(
+  <ErrorCatcher>
+    <LifecycleDemo />
+  </ErrorCatcher>,
+  document.querySelector("#root")
+);
