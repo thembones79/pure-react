b02b22c3c8e39cd1f361aa1d28d3411064953c0d
thembones79 <michal.szumnarski@gmail.com>
2018-11-19 23:15:35 +0100

    skeleton works

--- shopper/src/App.js
+++ shopper/src/App.js
@@ -3,14 +3,27 @@ import Nav from "./Nav";
 import "./App.css";
 
 class App extends React.Component {
+  state = { activeTab: 0 };
+
+  handleTabChange = index => {
+    this.setState({ activeTab: index });
+  };
+
   renderContent() {
-    return <span>Empty</span>;
+    switch (this.state.activeTab) {
+      default:
+      case 0:
+        return <span>Items</span>;
+      case 1:
+        return <span>Cart</span>;
+    }
   }
 
   render() {
+    let { activeTab } = this.state;
     return (
       <div className="App">
-        <Nav />
+        <Nav activeTab={activeTab} onTabChange={this.handleTabChange} />
         <main className="App-content">{this.renderContent()}</main>
       </div>
     );
--- shopper/src/Nav.js
+++ shopper/src/Nav.js
@@ -1,13 +1,13 @@
 import React from "react";
 
-const Nav = () => (
+const Nav = ({ activeTab, onTabChange }) => (
   <nav className="App-nav">
     <ul>
-      <li className="App-nav-item">
-        <span>Items</span>
+      <li className={`App-nav-item ${activeTab === 0 && "selected"}`}>
+        <a onClick={() => onTabChange(0)}>Items</a>
       </li>
-      <li className="App-nav-item">
-        <span>Cart</span>
+      <li className={`App-nav-item ${activeTab === 1 && "selected"}`}>
+        <a onClick={() => onTabChange(1)}>Cart</a>
       </li>
     </ul>
   </nav>
