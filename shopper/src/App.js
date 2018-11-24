ea64aa4ae668a152ee6351cd1671c6212d7b6cfc
thembones79 <michal.szumnarski@gmail.com>
2018-11-24 20:05:50 +0100

    works

--- shopper/src/App.js
+++ shopper/src/App.js
@@ -1,49 +1,61 @@
 import React from "react";
-import CartPage from './CartPage';
+import CartPage from "./CartPage";
 import Nav from "./Nav";
 import "./App.css";
-import ItemPage from './ItemPage';
-import {items} from './static-data';
+import ItemPage from "./ItemPage";
+import { items } from "./static-data";
 
 class App extends React.Component {
-  state = { activeTab: 0,
-    cart: []
-   };
+  state = { activeTab: 0, cart: [] };
 
   handleTabChange = index => {
     this.setState({ activeTab: index });
   };
 
-  handleAddToCart = (item) => {
+  handleAddToCart = item => {
     this.setState({
       cart: [...this.state.cart, item.id]
     });
-  }
+  };
 
-  renderCart(){
+  handleRemoveOne = item => {
+    let index = this.state.cart.indexOf(item.id);
+    this.setState({
+      cart: [
+        ...this.state.cart.slice(0, index),
+        ...this.state.cart.slice(index + 1)
+      ]
+    });
+  };
+
+  renderCart() {
     let itemCounts = this.state.cart.reduce((itemCounts, itemId) => {
       itemCounts[itemId] = itemCounts[itemId] || 0;
       itemCounts[itemId]++;
       return itemCounts;
-    },{});
+    }, {});
 
     let cartItems = Object.keys(itemCounts).map(itemId => {
       var item = items.find(item => item.id === parseInt(itemId, 10));
       return {
         ...item,
         count: itemCounts[itemId]
-      }
+      };
     });
-return (
-  <CartPage items={cartItems} />
-);
+    return (
+      <CartPage
+        items={cartItems}
+        onAddOne={this.handleAddToCart}
+        onRemoveOne={this.handleRemoveOne}
+      />
+    );
   }
 
   renderContent() {
     switch (this.state.activeTab) {
       default:
       case 0:
-        return <ItemPage items={items} onAddToCart={this.handleAddToCart}/>;
+        return <ItemPage items={items} onAddToCart={this.handleAddToCart} />;
       case 1:
         return this.renderCart();
       case 2:
@@ -56,9 +68,7 @@ return (
     return (
       <div className="App">
         <Nav activeTab={activeTab} onTabChange={this.handleTabChange} />
-        <div>
-          {this.state.cart.length} items
-        </div>
+        <div>{this.state.cart.length} items</div>
         <main className="App-content">{this.renderContent()}</main>
       </div>
     );
--- shopper/src/ItemPage.js
+++ shopper/src/ItemPage.js
@@ -1,14 +1,21 @@
 import React from "react";
 import PropTypes from "prop-types";
-import Item from './Item';
+import Item from "./Item";
 import "./ItemPage.css";
 
 function ItemPage({ items, onAddToCart }) {
   return (
     <ul className="ItemPage-items">
       {items.map(item => (
-        <li key={item.id} className="ItemPage=item">
-          <Item item={item} onAddToCart={() => onAddToCart(item)}/>
+        <li key={item.id} className="ItemPage-item">
+          <Item item={item}>
+            <button
+              className="Item-addToCart"
+              onClick={() => onAddToCart(item)}
+            >
+              Add to Cart
+            </button>
+          </Item>
         </li>
       ))}
     </ul>
