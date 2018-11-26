baeecf02837ff80db6a3edabd92d015808ae44a6
thembones79 <michal.szumnarski@gmail.com>
2018-11-26 23:16:51 +0100

    first commit

--- simplified-reddit/src/index.js
+++ simplified-reddit/src/index.js
@@ -7,13 +7,21 @@ class Reddit extends React.Component {
     state = {
         posts: []
     }
+
+    componentDidMount() {
+        axios.get(`https://www.reddit.com/r/reactjs.json`)
+        .then(res => {
+            const posts = res.data.data.children.map(obj => obj.data);
+            this.setState({posts});
+        });
+    }
     render(){
         return (
             <div>
                 <h1>/r/reactjs</h1>
                 <ul>
                 {this.state.posts.map(post => (
-                    <li key={post.id}>{post.title}</li>
+                    <li key={post.id}><hr /><h3>{post.author}</h3><div>score: {post.score}</div><a href={post.url}>{post.title}</a></li>
                 ))}
                 </ul>
             </div>
