b7069b28b644ba744982cc933ba980adc292a925
thembones79 <michal.szumnarski@gmail.com>
2018-11-26 20:42:26 +0100

    some testing

--- shop-cart-source/src/SourceJSON.js
+++ shop-cart-source/src/SourceJSON.js
@@ -1,8 +1,21 @@
 import React from "react";
 import { items } from "./static-data";
 
-const SourceJSON = () => (
-    <div>Here it will be</div>
-);
+class SourceJSON extends React.Component {
+  state = { dataFeed: JSON.stringify(items) };
+
+  handleDataChange = event => {
+    this.setState({ dataFeed: event.target.value });
+  };
+  render() {
+    return (
+      <textarea
+        id="editor"
+        value={this.state.dataFeed}
+        onChange={this.handleDataChange}
+      />
+    );
+  }
+}
 
 export default SourceJSON;
