46a846844829fb64e976668a24d1d4132d917b9e
thembones79 <michal.szumnarski@gmail.com>
2018-12-10 14:54:20 +0100

    first commit

---
+++ javascript-calculator/src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+const Calculator = () => <div className="calculator">Hi</div>;
+
+ReactDOM.render(<Calculator />, document.getElementById("root"));
