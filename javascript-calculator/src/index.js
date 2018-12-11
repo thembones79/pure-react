eda1cf37f250cd7b0ccf3d71766ace1348d7727d
thembones79 <michal.szumnarski@gmail.com>
2018-12-11 23:01:46 +0100

    clicks

--- javascript-calculator/src/index.js
+++ javascript-calculator/src/index.js
@@ -2,28 +2,100 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-const Calculator = () => (
+class Calculator extends React.Component {
+  state = {
+    currentNumber: "0",
+    formula: "",
+    result: "",
+    leftValue: "",
+    rightValue: "",
+    operator: "none"
+  };
+  handleValueClick = event => {
+    this.setState({
+      currentNumber:
+        this.state.currentNumber === "0"
+          ? event.target.value
+          : this.state.currentNumber + event.target.value
+    });
+  };
 
-<div id="calculator">
-    <div id="display">12345</div>    
-        <button id="zero" value={0}>0</button>
-        <button id="one" value={1}>1</button>
-        <button id="two" value={2}>2</button>
-        <button id="three" value={3}>3</button>
-        <button id="four" value={4}>4</button>
-        <button id="five" value={5}>5</button>
-        <button id="six" value={6}>6</button>
-        <button id="seven" value={7}>7</button>
-        <button id="eight" value={8}>8</button>
-        <button id="nine" value={9}>9</button>
-        <button id="add" value="+">+</button>
-        <button id="subtract" value="-">-</button>
-        <button id="multiply" value="x">&times;</button>
-        <button id="divide" value="/">&divide;</button>
-        <button id="decimal" value=".">.</button>
-        <button id="clear" value="C">C</button>
-        <button id="equals" value="=">=</button>    
-</div>
-);
+  handleDecimalClick = event => {
+    this.setState({
+      currentNumber: this.state.currentNumber.includes(".")
+        ? this.state.currentNumber
+        : this.state.currentNumber + event.target.value
+    });
+  };
 
-ReactDOM.render(<Calculator />, document.getElementById("root"));
+  handleAddClick = event => {
+    this.setState({
+      currentNumber: this.state.currentNumber[this.state.currentNumber.length-1]==="+"
+        ? this.state.currentNumber
+        : this.state.currentNumber + event.target.value
+    });
+  };
+
+  render() {
+    return (
+      <div id="calculator">
+        <div id="display">{this.state.currentNumber}</div>
+        <button onClick={this.handleValueClick} id="zero" value={0}>
+          0
+        </button>
+        <button onClick={this.handleValueClick} id="one" value={1}>
+          1
+        </button>
+        <button onClick={this.handleValueClick} id="two" value={2}>
+          2
+        </button>
+        <button onClick={this.handleValueClick} id="three" value={3}>
+          3
+        </button>
+        <button onClick={this.handleValueClick} id="four" value={4}>
+          4
+        </button>
+        <button onClick={this.handleValueClick} id="five" value={5}>
+          5
+        </button>
+        <button onClick={this.handleValueClick} id="six" value={6}>
+          6
+        </button>
+        <button onClick={this.handleValueClick} id="seven" value={7}>
+          7
+        </button>
+        <button onClick={this.handleValueClick} id="eight" value={8}>
+          8
+        </button>
+        <button onClick={this.handleValueClick} id="nine" value={9}>
+          9
+        </button>
+        <button onClick={this.handleAddClick} id="add" value="+">
+          +
+        </button>
+        <button onClick={this.handleSubtractClick} id="subtract" value="-">
+          -
+        </button>
+        <button onClick={this.handleMultiplyClick} id="multiply" value="*">
+          &times;
+        </button>
+        <button onClick={this.handleDivideClick} id="divide" value="/">
+          &divide;
+        </button>
+        <button onClick={this.handleDecimalClick} id="decimal" value=".">
+          .
+        </button>
+        <button onClick={this.handleClearClick} id="clear" value="C">
+          C
+        </button>
+        <button onClick={this.handleEqualsClick} id="equals" value="=">
+          =
+        </button>
+      </div>
+    );
+  }
+}
+
+const Dupa = () => <Calculator />;
+
+ReactDOM.render(<Dupa />, document.getElementById("root"));
