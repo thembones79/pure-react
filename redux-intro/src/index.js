67f85d00925c6314f0dde0209a5b108a54f869df
thembones79 <michal.szumnarski@gmail.com>
2018-12-22 18:22:26 +0100

    added redux

--- redux-intro/src/index.js
+++ redux-intro/src/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import {render} from 'react-dom';
-import './index.css';
-import Counter from './Counter';
+import React from "react";
+import { render } from "react-dom";
+import "./index.css";
+import Counter from "./Counter";
 
 const App = () => (
-    <div>
-        <Counter/>
-    </div>
+  <div>
+    <Counter />
+  </div>
 );
 
-render(<App/>,document.getElementById('root'));
+render(<App />, document.getElementById("root"));
