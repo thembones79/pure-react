1c4f6e6afd9ee9f4dfb344f3457e000550b9a097
thembones79 <michal.szumnarski@gmail.com>
2018-12-22 18:20:21 +0100

    first commit

---
+++ redux-intro/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {render} from 'react-dom';
+import './index.css';
+import Counter from './Counter';
+
+const App = () => (
+    <div>
+        <Counter/>
+    </div>
+);
+
+render(<App/>,document.getElementById('root'));
