ae0a59f11bee0372983c43a249ecc61e58a408e8
thembones79 <michal.szumnarski@gmail.com>
2018-12-22 23:29:29 +0100

    example to learn

--- redux-intro/src/Counter.js
+++ redux-intro/src/Counter.js
@@ -1,18 +1,21 @@
 import React from "react";
+import {connect} from 'react-redux';
 
 class Counter extends React.Component {
-  state = { count: 0 };
+ // state = { count: 0 };
 
   increment = () => {
-    this.setState({
-      count: this.state.count + 1
-    });
+    //  this.setState({
+    //    count: this.state.count + 1
+    //  });
+    this.props.dispatch({type: 'INCREMENT'});
   };
 
   decrement = () => {
-    this.setState({
-      count: this.state.count - 1
-    });
+    //   this.setState({
+    //    count: this.state.count - 1
+    //   });
+    this.props.dispatch({type: 'DECREMENT'});
   };
 
   render() {
@@ -21,7 +24,7 @@ class Counter extends React.Component {
         <h2>Counter</h2>
         <div>
           <button onClick={this.decrement}>-</button>
-          <span>{this.state.count}</span>
+          <span>{this.props.count}</span>
           <button onClick={this.increment}>+</button>
         </div>
       </div>
@@ -29,4 +32,10 @@ class Counter extends React.Component {
   }
 }
 
-export default Counter;
+function mapStateToProps(state){
+    return{
+        count: state.count
+    };
+}
+
+export default connect(mapStateToProps)(Counter);
--- redux-intro/src/index.js
+++ redux-intro/src/index.js
@@ -2,11 +2,34 @@ import React from "react";
 import { render } from "react-dom";
 import "./index.css";
 import Counter from "./Counter";
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+
+const initialState = {
+    count:0
+};
+
+function reducer(state = initialState, action) {
+    switch(action.type){
+        case 'INCREMENT':
+        return{
+            count: state.count + 1
+        };
+        case 'DECREMENT':
+        return{
+            count: state.count - 1
+        };
+        default:
+        return state;
+    }
+}
+
+const store = createStore(reducer);
 
 const App = () => (
-  <div>
+  <Provider store={store}>
     <Counter />
-  </div>
+  </Provider>
 );
 
 render(<App />, document.getElementById("root"));
