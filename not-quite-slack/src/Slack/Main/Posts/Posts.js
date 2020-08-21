82278dd915e2220d954c8b42ed3acf5833006c84
thembones79 <michal.szumnarski@gmail.com>
2020-08-21 13:58:06 +0200

    linter 's corrections

--- not-quite-slack/src/Slack/Main/Posts/Posts.js
+++ not-quite-slack/src/Slack/Main/Posts/Posts.js
@@ -1,32 +1,32 @@
-import React from 'react';
-import './Posts.css'
-import Post from './Post/Post';
+import React from "react";
+import "./Posts.css";
+import Post from "./Post/Post";
 
-const Posts = props => {
-    if (props.activeChannel === "none") {
-      return (
-        <div className="nothing-selected">
-          <p>Please select a channel or person to chat with.</p>
-        </div>
-      );
-    } else {      
-      let activeConversation = props.data[props.activeGroup].find(
-        active => active.channel === props.activeChannel
-      );
-  
-      return (
-        <div className="posts">
-          {activeConversation.messages.map((post, index) => (
-            <Post
-              key={index}
-              user={post.user}
-              timestamp={post.timestamp}
-              message={post.message}
-            />
-          ))}
-        </div>
-      );
-    }
-  };
+const Posts = (props) => {
+  if (props.activeChannel === "none") {
+    return (
+      <div className="nothing-selected">
+        <p>Please select a channel or person to chat with.</p>
+      </div>
+    );
+  } else {
+    let activeConversation = props.data[props.activeGroup].find(
+      (active) => active.channel === props.activeChannel
+    );
+
+    return (
+      <div className="posts">
+        {activeConversation.messages.map((post, index) => (
+          <Post
+            key={index}
+            user={post.user}
+            timestamp={post.timestamp}
+            message={post.message}
+          />
+        ))}
+      </div>
+    );
+  }
+};
 
 export default Posts;
